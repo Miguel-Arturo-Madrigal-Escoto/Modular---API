52fd3e6d9fa2d7e7b618785042a554df55c62912
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2023-06-20 22:31:05 -0600

    update in user and profile models and migrations

--- authentication/models.py
+++ authentication/models.py
@@ -2,16 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from .managers import CustomUserManager
 
-
-# Create your models here.
-class Profile(models.Model):
-    position = models.CharField(max_length=50)
-    expected_salary = models.FloatField()
-    modality = models.CharField(max_length=10)
-    location = models.CharField(max_length=50)
-    image = models.TextField(null=True)
-
-
 class User(AbstractBaseUser):
     username = models.CharField(
         unique=True,
@@ -25,8 +15,7 @@ class User(AbstractBaseUser):
     name = models.CharField(max_length=30)
     lastname = models.CharField(max_length=30)
     is_active = models.BooleanField(default=False) # email verification
-    google_id = models.CharField(max_length=255, null=True)
-    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name='user')
+    #google_id = models.CharField(max_length=255, null=True)
     
     # django required fields
     is_staff = models.BooleanField(default=False)
@@ -36,3 +25,13 @@ class User(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'name', 'lastname', 'password', 'profile']
+
+# Create your models here.
+class Profile(models.Model):
+    position = models.CharField(max_length=50)
+    expected_salary = models.FloatField()
+    modality = models.CharField(max_length=10)
+    location = models.CharField(max_length=50)
+    image = models.TextField(null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
--- authentication/serializers.py
+++ authentication/serializers.py
@@ -2,19 +2,21 @@ from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
 from .models import User, Profile
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
 class MyUserCreateSerializer(UserCreateSerializer):
+    profile = ProfileSerializer(read_only=True)
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ['username', 'email', 'password', 'name', 'lastname', 'profile']
 
 class MyCurrentUserSerializer(UserCreateSerializer):
+    profile = ProfileSerializer(read_only=True)
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ['id', 'username', 'email', 'name', 'lastname', 'profile']
 
-class ProfileSerializer(serializers.ModelSerializer):
-    user = MyUserCreateSerializer(read_only=True)
-    class Meta:
-        model = Profile
-        fields = '__all__'
 
