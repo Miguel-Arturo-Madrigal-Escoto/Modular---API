f2ff27f26ac25e94988186332e61645a0705f933
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2023-08-12 18:09:41 -0600

    permissions

--- skills/views.py
+++ skills/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Skill
@@ -9,6 +10,6 @@ from .serializers import SkillSerializer
 class SkillViewSet(ModelViewSet):
     queryset = Skill.objects.all()
     serializer_class = SkillSerializer
-    # permission_classes = [IsAuthenticated,]
+    permission_classes = (IsAuthenticated,)
     filter_backends = [DjangoFilterBackend]
     filterset_fields = '__all__'
