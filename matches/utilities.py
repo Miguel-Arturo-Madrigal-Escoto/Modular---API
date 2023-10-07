42750486ecce52d7f3bdb70e9ca9580e36446d52
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2023-10-06 21:01:24 -0600

    fixed typo

--- matches/utilities.py
+++ matches/utilities.py
@@ -11,13 +11,13 @@ class AlterMatchLikes:
     @staticmethod
     def alter_user(user: User):
         """
-            Remove the dislike from the company from the company if my role is 'user'.
+            Remove the dislike from the company if my role is 'user'.
         """
         Match.objects.filter(Q(company_like=False) | Q(company_like__isnull=True), user=user).update(company_like=None)
 
     @staticmethod
     def alter_company(company: Company):
         """
-            Remove the dislike from the user from the company if my role is 'company'.
+            Remove the dislike from the user if my role is 'company'.
         """
         Match.objects.filter(Q(user_like=False) | Q(user_like__isnull=True), company=company).update(user_like=None)
